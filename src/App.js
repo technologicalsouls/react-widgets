3449930907a8f9abadd0c453035240fa7dc9821e
technologicalsouls <technologicalsouls@gmail.com>
2020-10-06 16:50:38 -0400

    all widgets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = [
+    {
+        label: 'color red',
+        value: 'red'
+    },
+    {
+        label: 'color green',
+        value: 'green'
+    },
+    {
+        label: 'color blue',
+        value: 'blue'
+    }
+];
+const items = [
+    {
+        title: 'what is react?',
+        content: 'description 1'
+    },
+    {
+        title: 'why use react?',
+        content: 'description 2'
+    },
+    {
+        title: 'how do you use react?',
+        content: 'description 3'
+    }
+];
+
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return (
+            <Accordion items={items} />
+        );
+    }
 }
 
-export default App;
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return (
+            <Dropdown />
+        );
+    }
+}
+
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return (
+            <Search />
+        );
+    }
+}
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return (
+            <Translate />
+        );
+    }
+}
+
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
+    return (
+        <div>
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange = {setSelected}
+
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+        </div>
+    );
+};
+
